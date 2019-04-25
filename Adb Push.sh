3382c14ea294996bbbbe0659acaedbf9a514e947
ranolfi <mranolfi@gmail.com>
2019-04-25 00:08:54 -0300

    Fix 'rrescan' script name in Adb Push's output

--- Adb Push.sh
+++ Adb Push.sh
@@ -1,8 +1,8 @@
 adb connect 192.168.0.8
 sleep 1
-adb push ./Transfers/* /sdcard/Transfers
+adb push /home/ranolfi/Desktop/Transfers/* /sdcard/Transfers
 echo ""
-echo "Remember to run 'rescan in Android Terminal as root."
-echo "The 'rescan' script is placed under /system/sbin".
+echo "Remember to run 'rrescan' in Android Terminal as root."
+echo "The 'rrescan' script is placed under /system/sbin".
 echo ""
 read -n 1 -s -r -p "Press any key to continue."
