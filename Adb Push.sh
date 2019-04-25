a73b079fe6dc27a7a52c05bfaad7621a51485809
ranolfi <mranolfi@gmail.com>
2019-04-25 00:49:39 -0300

    Update directory structure and rename 'rrescan' script to 'rrscan'

--- Adb Push.sh
+++ Adb Push.sh
@@ -2,7 +2,7 @@ adb connect 192.168.0.8
 sleep 1
 adb push /home/ranolfi/Desktop/Transfers/* /sdcard/Transfers
 echo ""
-echo "Remember to run 'rrescan' in Android Terminal as root."
-echo "The 'rrescan' script is placed under /system/sbin".
+echo "Remember to run 'rrscan' in Android Terminal as root."
+echo "The 'rrscan' script is placed under /system/sbin".
 echo ""
 read -n 1 -s -r -p "Press any key to continue."
